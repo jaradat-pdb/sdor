aca5f6340e48d0c6444813309518180177f10447
jaradat-pdb <jaradat.pdb@gmail.com>
2020-03-20 01:29:39 +0200

    Add new data models and repositories

---
+++ sdor-webapp/src/main/java/org/pdbcorp/apps/sdor/data/model/Role.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 PDB Corp.
+ *
+ * Proprietary Software built off of open-source software?
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.pdbcorp.apps.sdor.data.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author jaradat-pdb
+ *
+ */
+@Document(collection = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+	@Transient
+	public static final String SEQUENCE_NAME = "roles_sequence";
+
+	@Id
+	private String id;
+
+	@Indexed(unique = true, direction = IndexDirection.DESCENDING)
+	private String name;
+
+	public Role(String name) {
+		this.name = name;
+	}
+}
